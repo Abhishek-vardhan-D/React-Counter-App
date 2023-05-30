5f95d6ee3cdbe25ee8b4a913cf6e06e5e195cfd1
root <abhishekvardhan003@gmail.com>
2023-05-30 04:50:01 +0000

    counter app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Welcome from './components/Welcome'
+import Counter from './components/Counter/index'
+
+const App = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+export default App
